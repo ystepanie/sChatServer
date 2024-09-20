9056d1fc3077bceb7b275c7c5e1f74423943f60a
ystepanie <ystepanie@naver.com>
2024-09-20 16:04:41 +0900

    - 랜덤 닉네임 설정 및 대화시 닉네임 표기 - 접속 시 해당 닉네임과 환영메시지 추가
    TODO
    - 채팅창 옆에 접속자 목록 표시하기

--- src/main/java/com/ideaproject/schatServer/controller/ClientController.java
+++ src/main/java/com/ideaproject/schatServer/controller/ClientController.java
@@ -24,13 +24,6 @@ import lombok.RequiredArgsConstructor;
 public class ClientController {
 	private final ClientService clientService;
 
-	//todo: 프로젝트 실행 시 서버 소켓 생성(서버 소켓은 while문을 통해 계속 클라이언트의 입장을 받아야 한다.)
-	//ㄴ 클라이언트 접속 시 스레드 생성
-	//todo: 전챗 방 들어가기
-	//ㄴ 방 접속 시 클라이언트 스레드 객체 생성
-	//ㄴ 메시지 입력하면 서버에서 받아서 클라이언트 스레드를 통해 뿌려줌
-	//ㄴ 접속 종료 시 스레드 종료
-
 	// 클라이언트 연결 api
 	@PostMapping(value = "/startClientConnect", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Response startClientConnect(@RequestBody ClientInitDto clientInitDto) throws Exception {
--- src/main/java/com/ideaproject/schatServer/handler/ChatHandler.java
+++ src/main/java/com/ideaproject/schatServer/handler/ChatHandler.java
@@ -1,15 +1,23 @@
 package com.ideaproject.schatServer.handler;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ChatHandler extends TextWebSocketHandler {
 	private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	private final static Logger log = LoggerFactory.getLogger(ChatHandler.class);
+	private ObjectMapper objectMapper = new ObjectMapper();
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -18,13 +26,41 @@ public class ChatHandler extends TextWebSocketHandler {
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		// JSON 메시지 파싱
+		Map<String, Object> payload = objectMapper.readValue(message.getPayload(), Map.class);
+
 		for (WebSocketSession webSocketSession : sessions) {
-			webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+			if ("nickname".equals(payload.get("type"))) {
+				// 닉네임 메시지 처리
+				String nickname = (String)payload.get("content");
+				session.getAttributes().put("nickname", nickname);
+				System.out.println("User connected with nickname: " + nickname);
+				webSocketSession.sendMessage(new TextMessage("Welcome " + nickname));
+			} else {
+				// 일반 채팅 메시지 처리
+				String chatMessage = (String) payload.get("content");
+				String nickname = (String) session.getAttributes().get("nickname");
+				System.out.println("Message from user: " + chatMessage);
+				 webSocketSession.sendMessage(new TextMessage(nickname + " : " + chatMessage));
+			}
 		}
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		log.info("afterConnectionClosed : {} remove", session.getId());
 		sessions.remove(session);
+		sendMessageToAll("user left");
+	}
+
+	// 전체 세션에 메시지 전송
+	private void sendMessageToAll(String message) {
+		for (WebSocketSession session : sessions) {
+			try {
+				session.sendMessage(new TextMessage(message));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
